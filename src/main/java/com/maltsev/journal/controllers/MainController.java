d456b752fd8c1dd5d741728a6eda587f0598f436
cinOPtic <artyommaltsev1@yandex.ru>
2024-05-09 20:05:21 +0300

    4 лаба Добавляем регистрацию и авторизацию пользователей в приложение

--- src/main/java/com/maltsev/journal/controllers/MainController.java
+++ src/main/java/com/maltsev/journal/controllers/MainController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 
 @Controller
 public class MainController {
